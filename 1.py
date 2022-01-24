ec8b6833df4227cb07ea17a63ed3c9bd367d6d2b
Fedor Ptitsyn <44300071+fptitsyn@users.noreply.github.com>
2022-01-24 18:23:19 +0300

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,11 @@
+x = 3
+y = 0
+
+if x < 0:
+    y = x
+elif x > y:
+    y = x * x
+else:
+    y = x - 1
+
+print(y)
---
+++ 2.py
@@ -0,0 +1,11 @@
+a = -3
+b = 8
+
+if a > b:
+    a = b
+else:
+    a += 3
+    b -= 1
+
+print(a)
+print(b)
